4c3d30ffe7669954388d294553e243a88327367b
NguyenNhuong <Nhuongblue@gmail.com>
2025-05-18 11:25:14 +0700

    Revert "Hoàn thiện chức năng đăng kí"
    This reverts commit ce346302355b2aaca405dcd93fcec717d4083621.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.example.smarthome"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 1
+        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
